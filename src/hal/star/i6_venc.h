c690df05d88157a2daf12cf99911d90a2026f35f
Unravel <7920887+peng_yuanzhuo@user.noreply.gitee.com>
2025-04-18 16:31:22 +0800

    feat(i6_venc): 添加帧内刷新功能
    - 新增 i6_venc_IntraRefresh 结构体用于配置帧内刷新参数
    - 在 i6_venc_impl 结构体中添加 fnSetIntraRefresh 和 fnGetIntraRefresh 函数指针
    - 在 i6_venc_load 函数中加载新的帧内刷新相关函数

--- src/hal/star/i6_venc.h
+++ src/hal/star/i6_venc.h
@@ -4,14 +4,16 @@
 
 #define I6_VENC_CHN_NUM 9
 
-typedef enum {
+typedef enum
+{
     I6_VENC_CODEC_H264 = 2,
     I6_VENC_CODEC_H265,
     I6_VENC_CODEC_MJPG,
     I6_VENC_CODEC_END
 } i6_venc_codec;
 
-typedef enum {
+typedef enum
+{
     I6_VENC_NALU_H264_PSLICE = 1,
     I6_VENC_NALU_H264_ISLICE = 5,
     I6_VENC_NALU_H264_SEI,
@@ -22,7 +24,8 @@ typedef enum {
     I6_VENC_NALU_H264_END
 } i6_venc_nalu_h264;
 
-typedef enum {
+typedef enum
+{
     I6_VENC_NALU_H265_PSLICE = 1,
     I6_VENC_NALU_H265_ISLICE = 19,
     I6_VENC_NALU_H265_VPS = 32,
@@ -32,7 +35,8 @@ typedef enum {
     I6_VENC_NALU_H265_END
 } i6_venc_nalu_h265;
 
-typedef enum {
+typedef enum
+{
     I6_VENC_NALU_MJPG_ECS = 5,
     I6_VENC_NALU_MJPG_APP,
     I6_VENC_NALU_MJPG_VDO,
@@ -40,14 +44,16 @@ typedef enum {
     I6_VENC_NALU_MJPG_END
 } i6_venc_nalu_mjpg;
 
-typedef enum {
+typedef enum
+{
     I6_VENC_SRC_CONF_NORMAL,
     I6_VENC_SRC_CONF_RING_ONE,
     I6_VENC_SRC_CONF_RING_HALF,
     I6_VENC_SRC_CONF_END
 } i6_venc_src_conf;
 
-typedef enum {
+typedef enum
+{
     I6_VENC_RATEMODE_H264CBR = 1,
     I6_VENC_RATEMODE_H264VBR,
     I6_VENC_RATEMODE_H264ABR,
@@ -62,7 +68,8 @@ typedef enum {
     I6_VENC_RATEMODE_END
 } i6_venc_ratemode;
 
-typedef enum {
+typedef enum
+{
     I6OG_VENC_RATEMODE_H264CBR = 1,
     I6OG_VENC_RATEMODE_H264VBR,
     I6OG_VENC_RATEMODE_H264QP,
@@ -76,7 +83,8 @@ typedef enum {
     I6OG_VENC_RATEMODE_END
 } i6og_venc_ratemode;
 
-typedef struct {
+typedef struct
+{
     unsigned int maxWidth;
     unsigned int maxHeight;
     unsigned int bufSize;
@@ -88,7 +96,8 @@ typedef struct {
     unsigned int refNum;
 } i6_venc_attr_h26x;
 
-typedef struct {
+typedef struct
+{
     unsigned int maxWidth;
     unsigned int maxHeight;
     unsigned int bufSize;
@@ -99,16 +108,19 @@ typedef struct {
     unsigned int markPerRow;
 } i6_venc_attr_mjpg;
 
-typedef struct {
+typedef struct
+{
     i6_venc_codec codec;
-    union {
+    union
+    {
         i6_venc_attr_h26x h264;
         i6_venc_attr_mjpg mjpg;
         i6_venc_attr_h26x h265;
     };
 } i6_venc_attrib;
 
-typedef struct {
+typedef struct
+{
     unsigned int gop;
     unsigned int statTime;
     unsigned int fpsNum;
@@ -117,7 +129,8 @@ typedef struct {
     unsigned int avgLvl;
 } i6_venc_rate_h26xcbr;
 
-typedef struct {
+typedef struct
+{
     unsigned int gop;
     unsigned int statTime;
     unsigned int fpsNum;
@@ -127,7 +140,8 @@ typedef struct {
     unsigned int minQual;
 } i6_venc_rate_h26xvbr;
 
-typedef struct {
+typedef struct
+{
     unsigned int gop;
     unsigned int fpsNum;
     unsigned int fpsDen;
@@ -135,7 +149,8 @@ typedef struct {
     unsigned int predQual;
 } i6_venc_rate_h26xqp;
 
-typedef struct {
+typedef struct
+{
     unsigned int gop;
     unsigned int statTime;
     unsigned int fpsNum;
@@ -144,21 +159,25 @@ typedef struct {
     unsigned int maxBitrate;
 } i6_venc_rate_h26xabr;
 
-typedef struct {
+typedef struct
+{
     unsigned int bitrate;
     unsigned int fpsNum;
     unsigned int fpsDen;
 } i6_venc_rate_mjpgcbr;
 
-typedef struct {
+typedef struct
+{
     unsigned int fpsNum;
     unsigned int fpsDen;
     unsigned int quality;
 } i6_venc_rate_mjpgqp;
 
-typedef struct {
+typedef struct
+{
     i6_venc_ratemode mode;
-    union {
+    union
+    {
         i6_venc_rate_h26xcbr h264Cbr;
         i6_venc_rate_h26xvbr h264Vbr;
         i6_venc_rate_h26xqp h264Qp;
@@ -174,32 +193,37 @@ typedef struct {
     void *extend;
 } i6_venc_rate;
 
-typedef struct {
+typedef struct
+{
     i6_venc_attrib attrib;
     i6_venc_rate rate;
 } i6_venc_chn;
 
-typedef struct {
+typedef struct
+{
     unsigned int quality;
     unsigned char qtLuma[64];
     unsigned char qtChroma[64];
     unsigned int mcuPerEcs;
 } i6_venc_jpg;
 
-typedef union {
+typedef union
+{
     i6_venc_nalu_h264 h264Nalu;
     i6_venc_nalu_mjpg mjpgNalu;
     i6_venc_nalu_h265 h265Nalu;
 } i6_venc_nalu;
 
-typedef struct {
+typedef struct
+{
     i6_venc_nalu packType;
     unsigned int offset;
     unsigned int length;
     unsigned int sliceId;
 } i6_venc_packinfo;
 
-typedef struct {
+typedef struct
+{
     unsigned long long addr;
     unsigned char *data;
     unsigned int length;
@@ -211,7 +235,8 @@ typedef struct {
     i6_venc_packinfo packetInfo[8];
 } i6_venc_pack;
 
-typedef struct {
+typedef struct
+{
     unsigned int leftPics;
     unsigned int leftBytes;
     unsigned int leftFrames;
@@ -224,7 +249,8 @@ typedef struct {
     unsigned int bitrate;
 } i6_venc_stat;
 
-typedef struct {
+typedef struct
+{
     unsigned int size;
     unsigned int skipMb;
     unsigned int ipcmMb;
@@ -241,7 +267,8 @@ typedef struct {
     unsigned int startQual;
 } i6_venc_strminfo_h264;
 
-typedef struct {
+typedef struct
+{
     unsigned int size;
     unsigned int iCu64x64;
     unsigned int iCu32x32;
@@ -253,28 +280,39 @@ typedef struct {
     unsigned int pCu4x4;
     unsigned int refType;
     unsigned int updAttrCnt;
-    unsigned int startQual;  
+    unsigned int startQual;
 } i6_venc_strminfo_h265;
 
-typedef struct {
+typedef struct
+{
     unsigned int size;
     unsigned int updAttrCnt;
     unsigned int quality;
 } i6_venc_strminfo_mjpg;
 
-typedef struct {
+typedef struct
+{
     i6_venc_pack *packet;
     unsigned int count;
     unsigned int sequence;
     int handle;
-    union {
+    union
+    {
         i6_venc_strminfo_h264 h264Info;
         i6_venc_strminfo_mjpg mjpgInfo;
         i6_venc_strminfo_h265 h265Info;
     };
 } i6_venc_strm;
 
-typedef struct {
+typedef struct
+{
+    char Enable;
+    unsigned int RefreshLineNum;
+    unsigned int ReqIQp;
+} i6_venc_IntraRefresh;
+
+typedef struct
+{
     void *handle;
 
     int (*fnCreateChannel)(int channel, i6_venc_chn *config);
@@ -293,9 +331,12 @@ typedef struct {
     int (*fnFreeStream)(int channel, i6_venc_strm *stream);
     int (*fnGetStream)(int channel, i6_venc_strm *stream, unsigned int timeout);
 
-    int (*fnQuery)(int channel, i6_venc_stat* stats);
+    int (*fnQuery)(int channel, i6_venc_stat *stats);
 
     int (*fnSetSourceConfig)(int channel, i6_venc_src_conf *config);
+
+    int (*fnSetIntraRefresh)(int channel, i6_venc_IntraRefresh *config);
+    int (*fnGetIntraRefresh)(int channel, i6_venc_IntraRefresh *config);
 
     int (*fnRequestIdr)(int channel, char instant);
     int (*fnStartReceiving)(int channel);
@@ -303,86 +344,97 @@ typedef struct {
     int (*fnStopReceiving)(int channel);
 } i6_venc_impl;
 
-static int i6_venc_load(i6_venc_impl *venc_lib) {
+static int i6_venc_load(i6_venc_impl *venc_lib)
+{
     if (!(venc_lib->handle = dlopen("libmi_venc.so", RTLD_LAZY | RTLD_GLOBAL)))
         HAL_ERROR("i6_venc", "Failed to load library!\nError: %s\n", dlerror());
 
-    if (!(venc_lib->fnCreateChannel = (int(*)(int channel, i6_venc_chn *config))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_CreateChn")))
+    if (!(venc_lib->fnCreateChannel = (int (*)(int channel, i6_venc_chn *config))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_CreateChn")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnDestroyChannel = (int(*)(int channel))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_DestroyChn")))
+    if (!(venc_lib->fnDestroyChannel = (int (*)(int channel))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_DestroyChn")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnGetChannelConfig = (int(*)(int channel, i6_venc_chn *config))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_GetChnAttr")))
+    if (!(venc_lib->fnGetChannelConfig = (int (*)(int channel, i6_venc_chn *config))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_GetChnAttr")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnGetChannelDeviceId = (int(*)(int channel, unsigned int *device))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_GetChnDevid")))
+    if (!(venc_lib->fnGetChannelDeviceId = (int (*)(int channel, unsigned int *device))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_GetChnDevid")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnResetChannel = (int(*)(int channel))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_ResetChn")))
+    if (!(venc_lib->fnResetChannel = (int (*)(int channel))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_ResetChn")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnSetChannelConfig = (int(*)(int channel, i6_venc_chn *config))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_SetChnAttr")))
+    if (!(venc_lib->fnSetChannelConfig = (int (*)(int channel, i6_venc_chn *config))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_SetChnAttr")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnFreeDescriptor = (int(*)(int channel))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_CloseFd")))
+    if (!(venc_lib->fnFreeDescriptor = (int (*)(int channel))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_CloseFd")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnGetDescriptor = (int(*)(int channel))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_GetFd")))
+    if (!(venc_lib->fnGetDescriptor = (int (*)(int channel))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_GetFd")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnGetJpegParam = (int(*)(int channel, i6_venc_jpg *param))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_GetJpegParam")))
+    if (!(venc_lib->fnGetJpegParam = (int (*)(int channel, i6_venc_jpg *param))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_GetJpegParam")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnSetJpegParam = (int(*)(int channel, i6_venc_jpg *param))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_SetJpegParam")))
+    if (!(venc_lib->fnSetJpegParam = (int (*)(int channel, i6_venc_jpg *param))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_SetJpegParam")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnFreeStream = (int(*)(int channel, i6_venc_strm *stream))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_ReleaseStream")))
+    if (!(venc_lib->fnFreeStream = (int (*)(int channel, i6_venc_strm *stream))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_ReleaseStream")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnGetStream = (int(*)(int channel, i6_venc_strm *stream, unsigned int timeout))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_GetStream")))
+    if (!(venc_lib->fnGetStream = (int (*)(int channel, i6_venc_strm *stream, unsigned int timeout))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_GetStream")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnQuery = (int(*)(int channel, i6_venc_stat *stats))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_Query")))
+    if (!(venc_lib->fnQuery = (int (*)(int channel, i6_venc_stat *stats))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_Query")))
         return EXIT_FAILURE;
 
-    venc_lib->fnSetSourceConfig = (int(*)(int channel, i6_venc_src_conf *config))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_SetInputSourceConfig");  
+    venc_lib->fnSetSourceConfig = (int (*)(int channel, i6_venc_src_conf *config))
+        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_SetInputSourceConfig");
 
-    if (!(venc_lib->fnRequestIdr = (int(*)(int channel, char instant))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_RequestIdr")))
+    if (!(venc_lib->fnSetIntraRefresh = (int (*)(int channel, i6_venc_IntraRefresh *config))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_SetIntraRefresh")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnStartReceiving = (int(*)(int channel))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_StartRecvPic")))
+    if (!(venc_lib->fnGetIntraRefresh = (int (*)(int channel, i6_venc_IntraRefresh *config))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_GetIntraRefresh")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnStartReceivingEx = (int(*)(int channel, int *count))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_StartRecvPicEx")))
+    if (!(venc_lib->fnRequestIdr = (int (*)(int channel, char instant))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_RequestIdr")))
         return EXIT_FAILURE;
 
-    if (!(venc_lib->fnStopReceiving = (int(*)(int channel))
-        hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_StopRecvPic")))
+    if (!(venc_lib->fnStartReceiving = (int (*)(int channel))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_StartRecvPic")))
+        return EXIT_FAILURE;
+
+    if (!(venc_lib->fnStartReceivingEx = (int (*)(int channel, int *count))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_StartRecvPicEx")))
+        return EXIT_FAILURE;
+
+    if (!(venc_lib->fnStopReceiving = (int (*)(int channel))
+              hal_symbol_load("i6_venc", venc_lib->handle, "MI_VENC_StopRecvPic")))
         return EXIT_FAILURE;
 
     return EXIT_SUCCESS;
 }
 
-static void i6_venc_unload(i6_venc_impl *venc_lib) {
-    if (venc_lib->handle) dlclose(venc_lib->handle);
+static void i6_venc_unload(i6_venc_impl *venc_lib)
+{
+    if (venc_lib->handle)
+        dlclose(venc_lib->handle);
     venc_lib->handle = NULL;
     memset(venc_lib, 0, sizeof(*venc_lib));
 }
